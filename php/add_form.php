de84180c48b825be0aa1463cb9410612d4d733c2
EbraheemAboraya <ibrahem.aboraya2002@gmail.com>
2023-05-16 00:12:14 +0300

    my first commit

---
+++ php/add_form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>add_form_php</title>
+</head>
+<body>
+<h3> Welcome to add_form </h3>    
+<h5> Object Name  :- <?php echo $_GET["Object Name "]; ?></h5>
+<h5> Building Name :-<?php echo $_GET["Building Name"]; ?></h5>
+<h5> Address Of Building :- <?php echo $_GET["Address Of Building"]; ?></h5>
+<h5> City :- <?php echo $_GET["City"]; ?></h5>
+<h4>Floor :- <?php echo $_GET["Floor"]; ?> </h5>
+<h4>Apartments :- <?php echo $_GET["Apartments"]; ?> </h5>
+
+    
+</body>
+</html>
